4ed0b253cee99ab26b543501b0184d3d96234fff
Yidnekachew-SK <yidnekachewsk@gmail.com>
2025-08-16 15:58:49 +0300

    Add more content
    Added images and corrected the project and todo list display functionality

--- src/content.js
+++ src/content.js
@@ -1,3 +1,8 @@
+import add from "./icons/add.svg";
+import remove from "./icons/delete.svg";
+import expand from "./icons/expand.svg";
+
+
 function createDiv(){
 	return document.createElement("div");
 }
@@ -8,6 +13,14 @@ function createParagraph(){
 
 function createButton(){
 	return document.createElement("button");
+}
+
+function createIcon(icon){
+	const img = document.createElement("img");
+	img.className = "icon";
+	img.src = `${icon}`;
+
+	return img;
 }
 
 function displayProject(project){
@@ -27,41 +40,73 @@ function displayProject(project){
 	titleContainer.append(title);
 
 	const addButton = createButton();
-	addButton.className = "add-todo";
+	addButton.className = "add-todo-button";
 	addButton.textContent = "+";
 	titleContainer.append(addButton);
-
-	showTodoList(project);
 }
 
 function showTodoList(project){
-	const projectDiv = document.querySelector(`.project-${project.name}`);
+	const listDisplayer = document.querySelector(".content-displayer");
 	for(let i = 0; i < project.todoList.length; i++){
+		const todoContainer = createDiv();
+		todoContainer.className = `todo-${project.todoList[i].name}`;
+		todoContainer.classList.add("todo-container");
+		listDisplayer.append(todoContainer);
+
+		const todoDisplayer = createDiv();
+		todoDisplayer.className = "todo-display";
+		todoContainer.append(todoDisplayer);
+
 		const nameOfTodo = createParagraph();
 		nameOfTodo.className = "todo-name";
 		nameOfTodo.textContent = `${project.todoList[i].name}`;
-		projectDiv.append(nameOfTodo);
+		todoDisplayer.append(nameOfTodo);
+
+		const expandButton = createButton();
+		expandButton.className = "expand-todo-button";
+		expandButton.textContent = ">";
+		todoDisplayer.append(expandButton);
+
+		const deleteButton = createButton();
+		deleteButton.className = "delete-todo-button";
+		deleteButton.textContent = "x";
+		todoDisplayer.append(deleteButton);
 	}
 }
 
-function displayTodos(todo){
-	const displayer = document.querySelector(".content-displayer");
-	const container = createDiv();
-	container.className = "todo-displayer";
-	displayer.append(container);
+function displayTodoDetails(todo){
+	const displayer = document.querySelector(`.todo-${todo.name}`);
 
-	const name = createParagraph();
-	name.textContent = `Name: ${todo.name}`;
-	container.append(name);
+	const detailDisplayer = createDiv();
+	detailDisplayer.className = "detail-displayer";
+	displayer.append(detailDisplayer);
 
-	const descrption = createParagraph();
-	descrption.textContent = `Description: ${todo.description}`;
-	container.append(descrption);
+	const description = createParagraph();
+	description.textContent = `Description: ${todo.description}`;
+	detailDisplayer.append(description);
 
 	const date = createParagraph();
 	date.textContent = `Due Date: ${todo.date}`;
-	container.append(date);
-
+	detailDisplayer.append(date);
 }
 
-export  {displayProject, displayTodos}
+function createProjectForm(){
+	const projectFormContainer = createDiv();
+	projectFormContainer.className = "project-form-container";
+	projectFormContainer.innerHTML = `
+	<form id="project-form" method = "POST">
+		<p>Add new project</p>
+		<div>
+			<label for="project-name">Project Name: </label><br>
+			<input type="text" id="project-name" name="Project-Name" required>
+		</div>
+		<div class = "project-form-button">
+			<button type="submit" class="create-project">Submit</button>
+			<button class="cancel-project">Cancel</button>
+		</div>
+	</form>
+	`;
+	document.body.append(projectFormContainer);
+}
+
+export {displayProject, showTodoList, displayTodoDetails, createProjectForm}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import {createTodo, createProject, Todo} from "./todo.js";
-import {displayProject, displayTodos} from "./content.js";
+import {displayProject, showTodoList, displayTodoDetails, createProjectForm} from "./content.js";
 
 const defaultProject = createProject("Demo");
 const a = createTodo("project", "complete todo app",21);
@@ -11,6 +11,12 @@ Todo.addToProject(defaultProject, b);
 Todo.addToProject(defaultProject, c);
 
 displayProject(defaultProject);
-displayTodos(a);
+showTodoList(defaultProject);
+displayTodoDetails(a);
 
 console.log(defaultProject);
+
+const addProject = document.querySelector(".add-project-button");
+addProject.addEventListener("click", () => {
+	createProjectForm();
+})
